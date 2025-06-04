459072f4fea5b2a7a3f1038b4b7efc4c152a8c43
Matt Mireles <4949789+mattmireles@users.noreply.github.com>
2025-06-03 18:57:16 -0700

    Initial Commit

---
+++ HyperChat/HyperChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperChatApp.swift
+//  HyperChat
+//
+//  Created by Matt Mireles on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HyperChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
