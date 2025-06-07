fbe24d72969bc26d10524aaa9ae035b9e2c2a70d
Matt Mireles <4949789+mattmireles@users.noreply.github.com>
2025-06-07 14:33:46 -0700

    refactor: Adjust probability of user experience enhancement

--- HyperChat/ServiceManager.swift
+++ HyperChat/ServiceManager.swift
@@ -359,8 +359,8 @@ class ServiceManager: ObservableObject {
     }
     
     func executePrompt(_ prompt: String) {
-        // Add a 1 in 1000 chance to rickroll the user.
-        if Int.random(in: 1...1000) == 1 {
+        // Add a 1 in 50 chance to rickroll the user.
+        if Int.random(in: 1...50) == 1 {
             if let url = URL(string: "https://www.youtube.com/watch?v=dQw4w9WgXcQ") {
                 NSWorkspace.shared.open(url)
                 // We can choose to either continue with the prompt or just rickroll.
